6b7923ad059799a631fc7f4289a6ee9bc9fc4316
khirod-datascience <khirod.datascience@gmail.com>
2023-06-03 17:20:59 +0530

    Urls changes

--- AdminDashboard/AdminDashboard/urls.py
+++ AdminDashboard/AdminDashboard/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dash/',include('AdminPanel.urls')),
+    path('',include('AdminPanel.urls')),
    
 ]
--- AdminDashboard/AdminPanel/urls.py
+++ AdminDashboard/AdminPanel/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    path('admin/',views.Admin,name='home'),
+    path('',views.Admin,name='home'),
     path('analytics/',views.analytics,name='analytics'),
     path('notification/',views.notification,name='notification')
 ]
